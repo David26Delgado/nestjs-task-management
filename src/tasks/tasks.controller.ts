6312e42db0aca5e6f940a5ae6c904b38c1ea5311
David Delgado <dadch13.14@gmail.com>
2020-05-03 11:16:58 -0400

    Persistence: Updating Task Status.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -46,12 +46,12 @@ export class TasksController {
   deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.tasksService.deleteTask(id);
   }
-  //
-  // @Patch(':id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  // ) {
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
+
+  @Patch(':id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 }
